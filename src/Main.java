8998bffa0433e5ee7565f3c393208e2f2b92eda9
unknown <241028>
2025-05-12 11:34:30 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Review1 {
+    public static void main(String[] args) {
+        System.out.print("こんにちは。");
+        System.out.println("今日の天気は" + "晴れです。");
+        System.out.println("今日は一日");
+        System.out.println();
+
+
+
+    }
+}
